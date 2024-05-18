1fe05bcfc71c7f95c68c48c6ebe6cfda4ffa07f1
fer-valicenti <129906298+fer-valicenti@users.noreply.github.com>
2024-05-18 00:16:44 -0300

    a ver
    esa

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        List<Driver> drivers= OpenF1Client.obtenerDriversDesdeAPI();
+        List<Circuit> circuits= OpenF1Client.obtenerCircuitosDesdeAPI();
+        F1JuegoGUI juegoGUI = new F1JuegoGUI(drivers, circuits);
+
+
     }
 }
